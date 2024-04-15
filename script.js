242a1411f3a32b055554445ff899504c36a96aae
juliomr21 <juliomr21.correo.com>
2024-04-15 20:05:33 -0300

    cadastro ok

--- script.js
+++ script.js
@@ -1,35 +1,34 @@
 function generar_cadastro() {
   let link_what = ['https://loja.produtosasos.com.br/precadastro/283582', 'https://loja.produtosasos.com.br/precadastro/202116']
-  const id = new Date().getUTCSeconds() % 2;
-  let link = link_what[id];
-  window.open(link);
+  const id = new Date().getUTCSeconds() % 3;
+  if (id == 0)
+    window.open(link_what[1]);
+  else
+    window.open(link_what[0]);
 }
 function generar_cel() {
-  let link_what = ' https://api.whatsapp.com/send?phone=559881921315'; 
+  let link_what = ' https://api.whatsapp.com/send?phone=559881921315';
   window.open(link_what);
 }
-function show_duvida(id_d){
-  if(document.getElementById('p'+ id_d).style.display == 'none')
-  {document.getElementById('p'+ id_d).style.display = 'block'}
-  else
-  {document.getElementById('p'+ id_d).style.display = 'none'}
+function show_duvida(id_d) {
+  if (document.getElementById('p' + id_d).style.display == 'none') { document.getElementById('p' + id_d).style.display = 'block' }
+  else { document.getElementById('p' + id_d).style.display = 'none' }
 }
-function go_to_section(seccionId){
+function go_to_section(seccionId) {
   var seccion = document.getElementById(seccionId);
   seccion.scrollIntoView();
 }
 
 window.onscroll = function () {
-  
+
   if (window.scrollY <= 550) {
-        document.getElementById('btn-whatsapp').style.display = 'none';
-  
-  }else
-  {
+    document.getElementById('btn-whatsapp').style.display = 'none';
+
+  } else {
     document.getElementById('btn-whatsapp').style.display = 'block';
   };
- 
-  }
+
+}
 
 
 
